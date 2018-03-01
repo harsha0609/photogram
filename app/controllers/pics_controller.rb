63509694eb2159018f4d2df9465e9311e73a7454
harsha0609 <hv2595@gmail.com>
2018-03-01 11:35:58 +0530

    show and index page

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,13 +1,23 @@
 class PicsController < ApplicationController
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
+
 	def index
 	end
 
+	def show
+	end
 	def new
 		@pic = Pic.new
 	end
 
 	def create
 		@pic = Pic.new(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: "Yess! it was posted1"
+		else
+			render 'new'
+		end
 	end
 
 	private
@@ -15,4 +25,9 @@ class PicsController < ApplicationController
 	def pic_params
 		params.require(:pic).permit(:title, :description)
 	end
+
+	def find_pic
+		@pic=Pic.find(params[:id])
+	end
+
 end
